7fb28c90a3e95c5d4f1f1bb54f016a756f51b58c
Guilherme Stéfano <guilhermenostefano@gmail.com>
2018-06-03 21:09:24 -0300

    added router to index.php

--- bookstore/index.php
+++ bookstore/index.php
@@ -1,22 +1,10 @@
 <?php 
 
+use Bookstore\Core\Router;
+use Bookstore\Core\Request;
 
-	require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-	$loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
-	$twig = new Twig_Environment($loader);
-
-	$bookModel = new \Bookstore\Models\BookModel(\Bookstore\Core\Db::getInstance());
-	$books = $bookModel->getAll(2,3);
-
-	$saleModel = new Bookstore\Models\SaleModel(Bookstore\Core\Db::getInstance());
-
-	$sales = $saleModel->getByUser(1);
-	$sale = $saleModel->get(1);
-
-	$params3 = ['sale' => $sale];
-	$params2 = ['sales' => $sales];
-
-	$params = ['books' => $books, 'currentPage' => 2];
-	echo $sales;
-	echo $twig->loadTemplate('sale.twig')->render($params2);
+$router = new Router();
+$response = $router->route(new Request());
+echo $response; 
